90f1b2a1b150668634a7be6a7cdadef55e0f3eb7
JonCoulter <jc803jc803@gmail.com>
2025-05-17 14:20:40 -0400

    added footer back

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import HomePage from './components/HomePage';
 import EventPage from './components/EventPage';
 import { AuthProvider } from './contexts/AuthContext';
@@ -18,6 +19,7 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/e/:eventId" element={<EventPage />} />
           </Routes>
+          <Footer />
         </Router>
       </AuthProvider>
     </ThemeProvider>
