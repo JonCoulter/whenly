ae318e5085c92ff3c67c7e7beb7631ed799d54d1
JonCoulter <jc803jc803@gmail.com>
2025-06-21 11:03:03 -0400

    fixed backup script path

--- backup_db.sh
+++ backup_db.sh
@@ -4,7 +4,7 @@
 # To set it up, run: crontab -e
 # And add this line: 0 3 * * * /home/jc803/whenly/backup_db.sh >> /home/jc803/whenly/backup_log.txt 2>&1
 
-cd /home/jc803/whenly || exit 1
+cd ~/whenly || exit 1
 
 # Backup the database
 make backup
